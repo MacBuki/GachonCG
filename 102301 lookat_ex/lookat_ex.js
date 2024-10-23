eab8e1ebb6c1a19636e07c0d85b8ddff5629ab7f
Ken Jeon <44134072+MacBuki@users.noreply.github.com>
2024-10-23 18:04:50 +0900

    1023

--- 102301 lookat_ex/lookat_ex.js
+++ 102301 lookat_ex/lookat_ex.js
@@ -15,51 +15,49 @@ var vertices = [
     vec3(0.5, -0.5, -0.5)  
 ];
 
-var vertexColors = [
-    vec4(1.0, 0.0, 0.0, 1.0), 
-    vec4(1.0, 0.0, 0.0, 1.0), 
-    vec4(1.0, 0.0, 0.0, 1.0), 
-    vec4(1.0, 0.0, 0.0, 1.0), 
-    vec4(1.0, 0.0, 0.0, 1.0), 
-    vec4(1.0, 0.0, 0.0, 1.0), 
-    vec4(1.0, 0.0, 0.0, 1.0), 
-    vec4(1.0, 0.0, 0.0, 1.0)  
+var faceColors = [
+    vec4(1.0, 0.0, 0.0, 1.0), // 빨강
+    vec4(0.0, 1.0, 0.0, 1.0), // 초록
+    vec4(0.0, 0.0, 1.0, 1.0), // 파랑
+    vec4(1.0, 1.0, 0.0, 1.0), // 노랑
+    vec4(1.0, 0.0, 1.0, 1.0), // 자홍
+    vec4(0.0, 1.0, 1.0, 1.0)  // 청록
 ];
 
-var radius = 1.0;
+var radius = 0.5;
 var theta = 0.0;
 var phi = 0.0;
 var dr = 5.0 * Math.PI / 180.0;
 
-var mvMatrix;
-var modelView;
+var mvMatrix, pMatrix;
+var modelView, projection;
 var eye;
 
 const at = vec3(0.0, 0.0, 0.0);
 const up = vec3(0.0, 1.0, 0.0);
 
-function quad(a, b, c, d) {
+function quad(a, b, c, d, color) {
     pointsArray.push(vertices[a]);
-    colorsArray.push(vertexColors[a]);
+    colorsArray.push(color);
     pointsArray.push(vertices[b]);
-    colorsArray.push(vertexColors[a]);
+    colorsArray.push(color);
     pointsArray.push(vertices[c]);
-    colorsArray.push(vertexColors[a]);
+    colorsArray.push(color);
     pointsArray.push(vertices[a]);
-    colorsArray.push(vertexColors[a]);
+    colorsArray.push(color);
     pointsArray.push(vertices[c]);
-    colorsArray.push(vertexColors[a]);
+    colorsArray.push(color);
     pointsArray.push(vertices[d]);
-    colorsArray.push(vertexColors[a]);
+    colorsArray.push(color);
 }
 
 function colorCube() {
-    quad(1, 0, 3, 2);
-    quad(2, 3, 7, 6);
-    quad(3, 0, 4, 7);
-    quad(6, 5, 1, 2);
-    quad(4, 5, 6, 7);
-    quad(5, 4, 0, 1);
+    quad(1, 0, 3, 2, faceColors[0]); // 앞면
+    quad(2, 3, 7, 6, faceColors[1]); // 오른쪽면
+    quad(3, 0, 4, 7, faceColors[2]); // 아래면
+    quad(6, 5, 1, 2, faceColors[3]); // 윗면
+    quad(4, 5, 6, 7, faceColors[4]); // 뒷면
+    quad(5, 4, 0, 1, faceColors[5]); // 왼쪽면
 }
 
 window.onload = function init() {
@@ -96,24 +94,29 @@ window.onload = function init() {
     gl.enableVertexAttribArray(vPosition);
 
     modelView = gl.getUniformLocation(program, "modelView");
+    projection = gl.getUniformLocation(program, "projection");
 
     document.getElementById("Button1").onclick = function () {
-        radius *= 1.1;
+        radius *= 1.1; console.log("radius:", radius); console.log("eye:", eye);
     };
     document.getElementById("Button2").onclick = function () {
-        radius *= 0.9;
+        radius *= 0.9; console.log("radius:", radius); console.log("eye:", eye);
     };
     document.getElementById("Button3").onclick = function () {
-        theta += dr;
+        theta += dr; console.log("theta:", theta); console.log("eye:", eye);
     };
     document.getElementById("Button4").onclick = function () {
-        theta -= dr;
+        theta -= dr; console.log("theta:", theta); console.log("eye:", eye);
     };
     document.getElementById("Button5").onclick = function () {
         phi += dr;
+        console.log("phi:", phi);
+        console.log("eye:", eye);
     };
     document.getElementById("Button6").onclick = function () {
         phi -= dr;
+        console.log("phi:", phi);
+        console.log("eye:", eye);
     };
 
     render();
@@ -121,11 +124,14 @@ window.onload = function init() {
 
 var render = function () {
     gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
+
+    // eye 벡터 계산
     eye = vec3(
-        radius * Math.sin(theta) * Math.cos(phi),
-        radius * Math.sin(phi),
+        radius * Math.cos(theta) * Math.sin(phi),
+        radius * Math.sin(theta),
         radius * Math.cos(theta) * Math.cos(phi)
     );
+
     mvMatrix = lookAt(eye, at, up);
     gl.uniformMatrix4fv(modelView, false, flatten(mvMatrix));
     gl.drawArrays(gl.TRIANGLES, 0, numVertices);
